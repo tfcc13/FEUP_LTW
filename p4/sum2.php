e0e2538eb33f6ef177626f9cbebd627cf6588cd8
Tiago <tiagofccarvalho13@gmail.com>
2024-03-13 09:59:32 +0000

    Made form to call php

--- p4/sum2.php
+++ p4/sum2.php
@@ -4,12 +4,16 @@
         return ($num1 + $num2);
     }
 
+
+    // Get, indicado para buscar informação que não é modificada. Exemplo subquery numa base de dados
+    // Post, indicado para processar informação que "é modificável", Exemplo, inserir, ou alterar dado numa base de dados
     $num1 = $_GET['num1'];
     $num2 = $_GET['num2'];
 
     echo 'This is number 1: ' .$num1 . '<br>'; // . são concatenadores
     echo 'This is number 1: ' .$num2 . '<br>'; // '<br>' interpreta o html
 
-    echo 'The sum of the values is: ' .sum_2_values($num1, $num2); 
+    echo 'The sum of the values is: ' .sum_2_values($num1, $num2) .'<br>'; 
 
+    echo '<a href="form2.html"> Go to form </a>';
 ?>
