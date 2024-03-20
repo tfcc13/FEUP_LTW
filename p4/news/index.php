99a570696cfd5bd536b3f53f3dea60a7620403c5
Tiago <tiagofccarvalho13@gmail.com>
2024-03-20 09:46:56 +0000

    Experimented some php commands for getting data

--- p4/news/index.php
+++ p4/news/index.php
@@ -3,14 +3,34 @@
     $db = new PDO('sqlite:news.db');
 
 
-    $stmt = $db->prepare('SELECT * FROM news');
-    $stmt->execute();
-    $articles = $stmt->fetchAll();
+    //$stmt = $db->prepare('SELECT * FROM news');
+    $all_data = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
+    FROM news JOIN
+         users USING (username) LEFT JOIN
+         comments ON comments.news_id = news.id
+    GROUP BY news.id, users.username
+    ORDER BY published DESC');
 
+    //$stmt->execute();
+    $all_data->execute();
+
+    $result = $all_data->fetchAll();
+    /*$articles = $stmt->fetchAll();
+    
     foreach ($articles as $article) {
         echo '<h1>' . $article['title'] . '</h1>';
         echo '<p>' . $article['introduction'] . '</p>';
 
     }
+    */
 
-?> 
+
+    foreach($result as $row) {
+        echo '<h1>' . $row['title'] . '</h1>';
+        echo '<p> ' . $row['introduction']. '</p>';
+        echo 'Author: ' . $row['username']. '<br>';
+        echo 'Comments: ' . $row['comments']. '<br>';
+    }
+
+
+?>
