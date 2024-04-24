04c43fde1049df9babc0a52f0a8fa41ab1bdd0e2
Tiago <tiagofccarvalho13@gmail.com>
2024-04-24 15:14:54 +0100

    Made exercise 3

--- p5/javascript/script.js
+++ p5/javascript/script.js
@@ -1,3 +1,14 @@
+/*
+// Exercise 1
+
+console.log('Hello Wordl');
+
+*/
+
+
+/*
+
+// EXERCISE 2
 function changeAllArticleColors() {
     const articles = document.querySelectorAll('#products article');
     
@@ -8,3 +19,30 @@ function changeAllArticleColors() {
 }
 
 changeAllArticleColors();
+*/
+
+
+function atachBuyEvents() {
+
+    // vai buscar todos os buttons dentro de product
+
+    const buttons = document.querySelectorAll('#products button')
+
+
+    /*
+    for loop em que dentro dos buttons itera cada button que quando carregado executa
+    uma função que é chamada pelo eventListener quando um botão é clicked
+    */
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            console.log('BUY');
+ 
+            //imprime o event Target, ou seja o button que foi clickado
+            console.log(this);
+        })
+    })
+
+
+}
+
+atachBuyEvents();
