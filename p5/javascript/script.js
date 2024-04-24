48f87976dd3b7840e2056c83be976ba29dd1d8a6
Tiago <tiagofccarvalho13@gmail.com>
2024-04-24 17:15:01 +0100

    Made exercise 5

--- p5/javascript/script.js
+++ p5/javascript/script.js
@@ -22,7 +22,7 @@ changeAllArticleColors();
 */
 
 
-function atachBuyEvents() {
+function attachBuyEvents() {
 
     // vai buscar todos os buttons dentro de product
 
@@ -63,12 +63,103 @@ function atachBuyEvents() {
 
             const  product_quant = article.querySelector('.quantity').value;        
             console.log('Chosen quantity:', product_quant);
+            
+            const total_price = product_quant*product_price;
+
+            const cart_table = document.querySelector('#cart table');
+
+            let cart_table_body = cart_table.querySelector('tbody');
+            if (!cart_table_body) {
+                cart_table_body = document.createElement('tbody');
+                cart_table.appendChild(cart_table_body);
+            }
 
 
-        })
+            const existing_row = document.querySelector(`#cart table tbody tr[data-id="${product_id}"]`);
+
+            if (existing_row) {
+                const quant_cell = existing_row.querySelector('.quantity');
+                const total_cell = existing_row.querySelector('.total');
+
+                const new_quant_cell = parseInt(quant_cell.textContent)+parseInt(product_quant);
+                const new_total_cell = total_price + parseInt(total_cell.textContent);
+
+                quant_cell.textContent = new_quant_cell;
+                total_cell.textContent = new_total_cell;
+            }
+
+            else {
+                
+                const new_row = document.createElement('tr');
+                new_row.setAttribute('data-id', product_id)
+
+                const id_cell = document.createElement('td');
+                id_cell.textContent = product_id;
+
+                const name_cell = document.createElement('td');
+                name_cell.textContent = product_name;
+                
+                const price_cell = document.createElement('td');
+                price_cell.textContent = product_price;
+
+                const quant_cell = document.createElement('td');
+                quant_cell.classList.add('quantity');
+                quant_cell.textContent = product_quant;
+                
+                const total_cell = document.createElement('td');
+                total_cell.textContent = total_price;
+                total_cell.classList.add('total');
+
+                const remove_cell_link = document.createElement('a');
+                remove_cell_link.href = '#';
+                remove_cell_link.textContent= 'X';
+                remove_cell_link.addEventListener('click', function(event) {
+                    event.preventDefault();
+                    new_row.remove();
+                    updateCart();
+                });
+
+                const remove_cell = document.createElement('td');
+                remove_cell.appendChild(remove_cell_link);
+                
+
+                new_row.appendChild(id_cell);
+                new_row.appendChild(name_cell);
+                new_row.appendChild(quant_cell);
+                new_row.appendChild(price_cell);
+                new_row.appendChild(total_cell);
+                new_row.appendChild(remove_cell);
+
+                const cart_table_body = cart_table.querySelector('tbody');
+                cart_table_body.appendChild(new_row);
+
+            }
+
+
+            
+            updateCart();
+
+        })      
     })
 
 
+    function updateCart() {
+        const rows = document.querySelectorAll('#cart table tbody tr');
+
+        let total = 0;
+
+        rows.forEach(row => {
+            const total_price = parseInt(row.querySelector('.total').textContent)
+            total += total_price;
+        });
+
+        total_element = document.querySelector('#cart table tfoot tr th:last-child');
+        
+        total_element.textContent = total;
+    }
+
 }
 
-atachBuyEvents();
+
+
+attachBuyEvents();
